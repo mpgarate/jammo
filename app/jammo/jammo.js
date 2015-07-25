92ab56beb178e6ff5c9a4f8a15eec9c64f3d88a3
Bill Garate <william.c.garate@gmail.com>
2015-07-25 14:43:15 -0400

    update app.js to working version

--- app/jammo/jammo.js
+++ app/jammo/jammo.js
@@ -11,7 +11,19 @@ angular.module('myApp.jammo', ['ngRoute'])
 
 .controller('JammoCtrl', ['$scope', function($scope) {
 
-  $scope.drum = "XXX";
-  console.log($scope.drum);
+  var beat1 = {
+    selected : false
+  };
+  var beat2 = {
+    selected : false
+  };
+  var beat3 = {
+    selected : false
+  };
+  var beat4 = {
+    selected : false
+  };
+
+  $scope.beats= [beat1, beat2, beat3, beat4];
 
 }]);
